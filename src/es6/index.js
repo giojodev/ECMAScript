e6af3852cb1cb0a77145b4494bfa0489fa1d144b
Giovanni Orozco <whoisyio@gmail.com>
2022-03-15 23:07:08 -0600

    let, const y multilinea

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,3 +21,42 @@ let epicPhrase=hello + ' ' +world;
 console.log(epicPhrase);
 let epicPhareES6= `${hello} ${world}`
 console.log(epicPhareES6)
+
+let lorem='asdweqdscdfewdcwfv wqesdacxvdewdf qscddsc \n'
++'asdfweqds qdsffre3wdsfef'
+console.log(lorem)
+
+//ES6
+let loremES6 =`Esto es una prueba
+bastante rigurosa
+para probar es6`
+
+console.log(loremES6)
+
+let person={
+    'name':'giovani',
+    'age':27,
+    'country':'NI'
+}
+console.log(person.name,person.age);
+
+//ES6
+let {name,age,country}=person;
+console.log(name,age,country);
+
+let team1=['oscar','julian','ricardo'];
+let team2=['valeria','yesica','camila'];
+
+let education =['david',...team1,...team2];
+
+console.log(education)
+
+{
+    var global='global var';
+}
+{
+    let globallet='global let'
+    console.log(globallet);
+}
+
+console.log(global);

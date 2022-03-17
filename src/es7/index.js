e92658e4f6d1a1538dbe1d3cb9a682eb2a3a188d
Giovanni Orozco <whoisyio@gmail.com>
2022-03-16 22:00:25 -0600

    Configuracion de ES7
    En ES7, se realiza configuracion prueba de metodo Includes y calculo de exponentes.

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+let numbers=[1,2,3,7,8];
+
+//ES7 genera el metodo includes para buscar dentro de un array un elemento
+if(numbers.includes(9)){
+    console.log('Si se encuentra el valor 7');
+
+} else{
+    console.log('No se encuentra')
+}
+
+let base=4;
+let exponent=4;
+//ES7 se calcula el exponencial de la siguiente manera
+let resultado=base**exponent;
+
+console.log(resultado);

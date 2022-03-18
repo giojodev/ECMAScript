c09bb4d00970054ce613023420349e6da0fc17c7
Giovanni Orozco <whoisyio@gmail.com>
2022-03-17 21:21:22 -0600

    Funciones Async y await

--- src/es8/index.js
+++ src/es8/index.js
@@ -26,3 +26,29 @@ console.log(string.padStart(7,'hi'));
 console.log(string.padEnd(7,'hi'));
 console.log('food'.padEnd(12,' ----- ' ));
 
+//Async Await
+const helloWorld=()=>{
+    return new Promise((resolve,reject)=>{
+        (true)
+        ? setTimeout(() => resolve('Hello World'),3000)
+        : reject(new Error('Test Error'))
+    })
+}
+
+const HelloAsync = async()=>{
+    const hello =await helloWorld();
+    console.log(hello);
+
+}
+
+HelloAsync();
+
+const anotherFunction = async()=>{
+    try {
+        const hello =await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+anotherFunction();

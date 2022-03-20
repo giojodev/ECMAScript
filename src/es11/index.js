f3308c5f98e38fda271e988a5b239486fc4a867b
Giovanni Orozco <whoisyio@gmail.com>
2022-03-19 22:48:49 -0600

    Update index.js

--- src/es11/index.js
+++ src/es11/index.js
@@ -3,4 +3,32 @@ const button=document.getElementById("btn");
 button.addEventListener('click',async function(){
     const module=await import("./file.js");
     module.hello();
-})
+});
+
+const bigNumber=9000901232332n;
+
+const anotherBigNumber=BigInt(1234124123123123123123);
+console.log(bigNumber);
+console.log(anotherBigNumber);
+
+// const promise1=new Promise((resolve,reject)=>reject("reject"));
+// const promise2=new Promise((resolve)=>resolve("resolve 1"));
+// const promise3=new Promise((resolve)=>resolve("resolve 2"));
+
+// Promise.allSettled(promise1,promise2,promise3)
+//     .then(response=>console.log(response)
+// console.log(window);
+console.log(globalThis);
+
+const fooo=null ?? 'default string';
+console.log(fooo);
+
+const user={};
+
+console.log(user?.profile?.email);
+
+if(user?.profile?.email){
+    console.log("Ttenemos email");
+}else{
+    console.log("fail");
+}
